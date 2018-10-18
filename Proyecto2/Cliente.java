efd345b10cd3836cc4550bef8fd6028d9ef07923
luisjaviermon <eleoteesdeliciosa@gmail.com>
2018-10-17 23:15:06 -0500

    Proyecto java intermedio

--- Proyecto2/Cliente.java
+++ Proyecto2/Cliente.java
@@ -1,69 +1,114 @@
-/*socket: punto final de una conexion (nodo)
-
-        ----------------------------->
-cliente								   Servidor
-        <-----------------------------
-(socket)							  (socket)
-
-para la conexion entre cliente y servidor se usara el protocolo TCP
-
-los clientes srien los hilos que utilizan el servicio*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
-
-/* para este caso el socket necesita la direccion ip y el puerto para poder enviar info*/
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.io.Serializable;
 
 public class Cliente{
-	private PrintStream ps;
-	private String host; //almacena la direccion ip
-	private int port;//almacena el puerto
+  private PrintStream clienteOut;
+  private String host;
+  private int port;
+  private String nick;
+  private String mi_ip;
+  
 
-	public void setHost(String host){
-		this.host = host;
-	}
+  public void setHost(String host){
+    this.host = host;
+  }
+  public void setPort(int port){
+    this.port = port;
+  }
+  public void setNick(String nick){
+    this.nick = nick;
+  }
 
-	public void setPort(int port){
-		this.port = port;
-	}
+  public String getNick(){
+      return nick;
+  } 
 
-	public BufferedReader conectar(BufferedReader entrada) throws IOException{
-		Socket s = new Socket(host,port);
-		ps = new PrintStream(s.getOutputStream());
-		entrada = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		return entrada;
-	}
+  public String getMiIp(){
+      return mi_ip;
+  } 
+  public BufferedReader conectar(BufferedReader entrada) throws IOException{
+    Socket server = new Socket(host,port);
+    mi_ip = reduce(server.getInetAddress().toString());
+    clienteOut = new PrintStream(server.getOutputStream());
+	entrada = new BufferedReader(new InputStreamReader(server.getInputStream()));
+	return entrada;
+	
+  }
+  public void enviar(String mensaje) throws IOException{
+    clienteOut.println(mensaje);
+  }
+  public void cerrar() throws IOException{
+    clienteOut.close();
+  }
 
-	public void enviar(String mensaje) throws IOException{
-		ps.println(mensaje);//envia un flujo hacia la salida estandar (consola)
+  public String reduce(String cad){
+    cad = cad.substring(1,cad.length());
+    return cad;
+  }
 
-	}
 
-	public void cerrar() throws IOException{
-		ps.close();
-	}
+  public static void main(String[] args) throws IOException{
+    BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+	BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+    Cliente cliente = new Cliente();
+    System.out.print("Host: ");
+    cliente.setHost(teclado.readLine());
+    System.out.print("Puerto: ");
+    cliente.setPort(Integer.parseInt(teclado.readLine()));
+    System.out.print("Nick: ");
+    cliente.setNick(teclado.readLine());
+    entrada = cliente.conectar(entrada);
+    String str = "";
+	String v = "";
+    int control = 0;
+	
+    ArrayList<String> IpsMenu = new ArrayList<String>();
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader teclado,entrada;
-		String viene;
-		teclado = new BufferedReader(new InputStreamReader(System.in));
-		entrada = new BufferedReader(new InputStreamReader(System.in));
-		Cliente c = new Cliente();
-		System.out.print("Host: ");
-		c.setHost(teclado.readLine());
-		System.out.print("Puerto: ");
-		c.setPort(Integer.parseInt(teclado.readLine()));
-		entrada = c.conectar(entrada);
-		String str = "";
-		while(!str.equals("salir")){
-			System.out.print("Mensaje: ");
-			str = teclado.readLine();
-			c.enviar(str);
-			viene = entrada.readLine();
-			System.out.println(viene);
-		}
-	}
+    System.out.println("\tEntraste al servidor");
+    System.out.println("Añadete al chat con el comando 'add'.");
+    while(control==0){
+    str = teclado.readLine();
+      if (str.equals("add")){
+
+        System.out.println("Usuario añadido");
+        cliente.enviar(cliente.getMiIp());
+        cliente.enviar(cliente.getNick());
+        do{
+        System.out.print("Escribe tu comando: ");
+          str = teclado.readLine();
+          if(str.equals("text")){
+          System.out.print("Mensaje: ");
+          str = teclado.readLine();
+          cliente.enviar(str);
+          } else if (str.equals("text_to")){
+            System.out.println("Mensaje privado");
+            
+
+          } else if (str.equals("list")){
+            System.out.println("Lista de usuarios:");
+            System.out.println(IpsMenu);
+            
+
+          } else if (str.equals("send_file")){
+            System.out.println("Enviar archivo");
+
+          } else{System.out.println("Comando incorrecto");}
+			v=entrada.readLine();
+			System.out.print(v);
+			
+        } while(!str.equals("quit"));
+        cliente.cerrar();
+        control=1;
+      } else {System.out.println("Error, usa el comando 'add' para añadirte al chat.");}
+
+    } 
+  }
 }
+
--- Proyecto2/Servidor.java
+++ Proyecto2/Servidor.java
@@ -1,58 +1,86 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.PrintStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.Serializable;
 import java.io.DataOutputStream;
 
 public class Servidor extends Thread{
-	private String nombreCliente;
-	private BufferedReader entrada;
-	private PrintStream salida;
+  private String nombreCliente;
+  private BufferedReader entrada;
+  private PrintStream salida;
+  public static ArrayList<PaqueteDatosCliente> lip = new ArrayList<PaqueteDatosCliente>();
+  public static ArrayList<Servidor> as = new ArrayList<Servidor>();
+  
+//agregando usuarios
 
-	public Servidor(Socket s) throws IOException{
-		entrada = new BufferedReader(new InputStreamReader(s.getInputStream()));//recibe el flujo de datos del socket cliente
-		salida = new PrintStream(s.getOutputStream());
-		nombreCliente = s.getInetAddress().getCanonicalHostName();
-		System.out.println("Conexion aceptada desde "+s.getRemoteSocketAddress());//obtenemos la ip del socket
-	}
+  public Servidor(Socket cliente) throws IOException{
+    entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+    salida = new PrintStream(cliente.getOutputStream());
+    System.out.println("Conexion aceptada desde: " + cliente.getRemoteSocketAddress());
+    PaqueteDatosCliente nu = new PaqueteDatosCliente();
+    nu.setMiIp(recibir());
+    nu.setNick(recibir());
+    nombreCliente=nu.getNick();
+    System.out.println("datos: "+nu.getMiIp()+" "+nu.getNick());
+    lip.add(nu);
 
-	public String recibir() throws IOException{
-		String str = entrada.readLine();
-		return str;
-	}
+    
+  }
 
-	public void cerrar() throws IOException{
-		entrada.close();
-	}
+  public String recibir() throws IOException{
+    String str = entrada.readLine();
+    return str;
+  }
+  public void cerrar() throws IOException{
+    entrada.close();
+  }
 
-	@Override
-	public void run(){
-		try{
-			salida.println("bienvenido");
-			String str = "";
-			while(!str.equals("salir")){
-				str = recibir();//obtiene lo enviado por el cliente
-				System.out.println(nombreCliente +" dice "+str);
-				salida.println("men");
-			}
-		}catch(IOException e){
-			System.out.println("se cerro la conexion con "+nombreCliente);
-		}finally{
-			try{
-				cerrar();
-			}catch(IOException e){}
-		}
-	}
+  public int conexionCio(String msn){
+      salida.println(msn);
+      return 1;
+  }
 
-	public static void main(String[] args) throws IOException{
-		ServerSocket ss = new ServerSocket(9998);//lo unico que necesita es el pueto en el cual aceptara la conexion
-		System.out.println("el puerto "+ss.getLocalPort()+" esta aceptando conexiones");
-		while(true){
-			Socket cliente = ss.accept();//suspende(espera) el proceso hasta encontrar una connexion(espera nuevas conexiones)
-			Servidor hilo = new Servidor(cliente);//encontrada crea el hilo para darle el servicio
-			hilo.start();//inicia el servicio unico para el cliente
-		}
-	}
+  @Override
+  public void run(){
+    try{
+      String str = "";
+      while(!str.equals("quit")){
+        str = recibir();
+        System.out.println(nombreCliente +" dice "+str);
+        System.out.println("usuarios");
+        for(int i =0;i<lip.size();i++){
+            as.get(i).conexionCio("Alguien envio: "+str+"\n");
+        }
+
+
+
+      }
+    }catch (IOException e) {
+      System.out.println("Se cerro la conexion con " + nombreCliente);
+    }finally{
+      try{
+        cerrar();
+      }catch(IOException ex){}
+    }
+  }
+  public static void main(String[] args) throws IOException{
+    ArrayList<PaqueteDatosCliente> ips = new ArrayList<PaqueteDatosCliente>();
+    ServerSocket ss;
+    ss = new ServerSocket(9998);
+    System.out.println("Servidor aceptando conexiones en el puerto " + ss.getLocalPort());
+    while(true){
+      System.out.println("esperando");
+      Socket cliente = ss.accept();
+      System.out.println("esperando2");
+      as.add(new Servidor(cliente));
+      as.get(as.size()-1).start();
+      ips=lip;
+            
+    }
+  }
 }
+
